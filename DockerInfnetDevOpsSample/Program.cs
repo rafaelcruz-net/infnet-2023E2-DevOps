82599e3c023e49472e8e137258bc53c06ce55e79
Rafael Cruz <rafaelcruz.net81@gmail.com>
2023-09-09 14:22:55 -0300

    feat: adicionando health check

--- DockerInfnetDevOpsSample/Program.cs
+++ DockerInfnetDevOpsSample/Program.cs
@@ -1,3 +1,8 @@
+
+
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Add Health Check
+builder.Services.AddHealthChecks()
+                .AddSqlServer(
+                    connectionString: builder.Configuration.GetConnectionString("InfnetPosDb"),
+                    healthQuery: "SELECT 1",
+                    name: "Database",
+                    failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Unhealthy
+                );
+
+builder.Services.AddHealthChecksUI( s =>
+{
+    s.AddHealthCheckEndpoint("Infnet API", "https://localhost:7122/healthz");
+})
+.AddInMemoryStorage();
+
 
 var app = builder.Build();
 
@@ -21,6 +43,18 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseRouting()
+   .UseEndpoints(config =>
+   {
+       config.MapHealthChecks("/healthz", new HealthCheckOptions
+       {
+           Predicate = _ => true,
+           ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+       });
+
+       config.MapHealthChecksUI();
+   });
 
 app.Run();
 
